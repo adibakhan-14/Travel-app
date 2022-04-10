92526aa3e77e68d3f21f5b55fe35fee88fd6f17e
Adiba_14 <adibasumaiya.27@gmail.com>
2022-04-11 00:37:42 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"
 
-function App() {
+
+
+export default function App(){
+  const [trips, setTrips]= useState([]) //first time rendering the app
+   const [url, setUrl]= useState('http://localhost:3000/trips')
+useEffect(()=>{
+  fetch(url).then((response)=> response.json()).then((data)=> setTrips(data))
+}, [url]) //first mount
+  console.log(trips);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+      <h1>
+        Travel Site
+      </h1>
+      <div className="filter-trips">
+      <button onClick={()=>setUrl("http://localhost:3000/trips")}>All trips</button>
+      <button onClick={()=> {setUrl("http://localhost:3000/trips?type=Water")}}>Boat trip</button>
+      <button  onClick={()=> {setUrl("http://localhost:3000/trips?type=Air")}}>Air trip</button>
+      <button  onClick={()=> {setUrl("http://localhost:3000/trips?type=Road")}}>Road trip</button>
+      </div>
+      <ul className="trip-list">
+        {trips.map((data) => (
+          <li >
+            <h2>
+              {data.title}
+            </h2>
+            <p>{data.price}</p>
+            <h3>{data.duration}</h3>
+          </li>
+        ))
+        }
+      </ul>
+      
+      
 
-export default App;
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// to fetch api we can use axios also
+//TASK- how to create custom HOOKS
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
